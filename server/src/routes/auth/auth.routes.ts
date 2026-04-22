c77f34ccd69c5c06cb6fe30676b6efaac1528083
Ilham Maslaw <ilhammaslaw1407@gmail.com>
2026-04-22 21:18:02 +0700

    add auth , route

---
+++ server/src/routes/auth/auth.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import { requireAuth } from "../../middleware/auth";
+import { asyncHandler } from "../../utils/asyncHandler";
+import { clerkClient, getAuth } from "@clerk/express";
+import { AppError } from "../../utils/AppError";
+
+
+export const authRouter = Router();
+
+authRouter.post('/sync', requireAuth,
+
+    asyncHandler(async (req, res) => {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+      throw new AppError(401, "User is not logged in. Means unauth user! !");
+    }
+
+    const clerkUser = await clerkClient.users.getUser(userId);
+
+    const extractEmailFromUserInfo =
+      clerkUser.emailAddresses.find(
+        (item) => item.id === clerkUser.primaryEmailAddressId,
+      ) || clerkUser.emailAddresses[0];
+
+    const email = extractEmailFromUserInfo.emailAddress;
+
+    const fullName = [clerkUser.firstName, clerkUser.lastName]
+      .filter(Boolean)
+      .join(" ")
+      .trim();
+
+    const name = fullName || clerkUser.username;
+
+    const raw = process.env.ADMIN_EMAILS || "";
+    const adminEmails = new Set(
+      raw
+        .split(",")
+        .map((item) => item.trim().toLowerCase())
+        .filter(Boolean),
+);
